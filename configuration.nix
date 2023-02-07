666a3c5563e7398f3483afa518906d26b951389f
MoritzRS <moritz.r.schulz@gmail.com>
2023-02-07 19:55:50 +0100

    added filesystems to setup

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,22 @@
     };
   };
 
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
+
   # Bootloader
   boot = {
     kernelParams = [

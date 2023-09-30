03c100a2f60f8f81c45fcc3fceadc207e3693112
MoritzRS <moritz.r.schulz@gmail.com>
2023-09-30 13:56:02 +0200

    added can touch efi vars

--- config/configuration.nix
+++ config/configuration.nix
@@ -17,6 +17,7 @@
   # Bootloader
   boot.kernelParams = ["quite" "loglevel=3"];
   boot.loader.timeout = 3;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
